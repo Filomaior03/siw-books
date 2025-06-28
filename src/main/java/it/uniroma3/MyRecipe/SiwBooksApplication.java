addba188bffd905a7a732b3829cac577725917b1
Filomaior03 <filomaior12@gmail.com>
2025-06-28 12:41:38 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/MyRecipe/SiwBooksApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.MyRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwBooksApplication.class, args);
+	}
+
+}
